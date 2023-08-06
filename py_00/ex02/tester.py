28a1ff7d4cabd64401fe144088dae42fa740a7ab
Tamas Tokesi <ttokesi@2-C-5.42heilbronn.de>
2023-08-06 19:09:53 +0200

    p1 0

--- py_00/ex02/tester.py
+++ py_00/ex02/tester.py
@@ -1,26 +1,27 @@
 from find_ft_type import all_thing_is_obj
 
-ft_list = ["Hello", "tata!"]
-ft_tuple = ("Hello", "toto!")
-ft_set = {"Hello", "tutu!"}
-ft_dict = {"Hello" : "titi!"}
+# ft_list = ["Hello", "tata!"]
+# ft_tuple = ("Hello", "toto!")
+# ft_set = {"Hello", "tutu!"}
+# ft_dict = {"Hello" : "titi!"}
 
-all_thing_is_obj(ft_list)
-all_thing_is_obj(ft_tuple)
-all_thing_is_obj(ft_set)
-all_thing_is_obj(ft_dict)
-all_thing_is_obj("Brian")
+# all_thing_is_obj(ft_list)
+# all_thing_is_obj(ft_tuple)
+# all_thing_is_obj(ft_set)
+# all_thing_is_obj(ft_dict)
+# all_thing_is_obj("Brian")
 
-print(all_thing_is_obj(10))
-
-
-print("\nmy own test:")
-class TestThisClass:
-    pass
-
-all_thing_is_obj(None);
-somethiing = TestThisClass()
-all_thing_is_obj(somethiing)
+# print(all_thing_is_obj(10))
+print(all_thing_is_obj(1.5))
 
 
-all_thing_is_obj(False)
+# print("\nmy own test:")
+# class TestThisClass:
+#     pass
+
+# all_thing_is_obj(None);
+# somethiing = TestThisClass()
+# all_thing_is_obj(somethiing)
+
+
+# all_thing_is_obj(False)
--- py_01/ex00/give_bmi.py
+++ py_01/ex00/give_bmi.py
@@ -1,6 +1,21 @@
 def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
-    pass
+    bmi = []
+    if len(height) != len(weight):
+        raise ValueError("lists doen't have the same number of values")
+    for num in height:
+        if not isinstance(num, (int, float)):
+            raise TypeError("Wrong type of vlaues in height")
+    for num in weight:
+        if not isinstance(num, (int, float)):
+            raise TypeError("Wrong type of vlaues in weight")
+    for h, w in zip(height, weight):
+        temp = w / (h * h)
+        bmi.append(temp)
+    return bmi
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
-    pass
+    for num in bmi:
+        if not isinstance(num, (int, float)):
+            raise TypeError("Wrong type of vlaues in list")
+    return [limit < num for num in bmi]
